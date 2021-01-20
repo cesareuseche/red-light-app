9eec151a41ed006bcb35ccb23cc5b36d6a34b115
cesareuseche <cesareuseche@gmail.com>
2021-01-20 17:59:16 -0400

    red light front end app finished

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,42 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [light, setLight] = useState("");
+
+	const redON = e => {
+		if (light != "red") {
+			setLight("red");
+		} else setLight("");
+	};
+	const yellowON = e => {
+		if (light != "yellow") {
+			setLight("yellow");
+		} else setLight("");
+	};
+	const greenON = e => {
+		if (light != "green") {
+			setLight("green");
+		} else setLight("");
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center mt-5 d-flex flex-column align-items-center">
+			<div className="stick" />
+			<div className="red__light__body">
+				<div
+					className={light == "red" ? "red lightON" : "red"}
+					onClick={redON}
+				/>
+				<div
+					className={light == "yellow" ? "yellow lightON" : "yellow"}
+					onClick={yellowON}
+				/>
+				<div
+					className={light == "green" ? "green lightON" : "green"}
+					onClick={greenON}
+				/>
+			</div>
 		</div>
 	);
 }
